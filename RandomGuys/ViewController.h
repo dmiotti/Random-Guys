6514fabd9310656d64f6b8df165c5c45ecc66860
David Miotti <david@muxumuxu.com>
2016-12-27 13:16:42 +0100

    Initial Commit

---
+++ RandomGuys/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomGuys
+//
+//  Created by David Miotti on 27/12/2016.
+//  Copyright © 2016 David Miotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RandomGuys/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RandomGuys
+//
+//  Created by David Miotti on 27/12/2016.
+//  Copyright © 2016 David Miotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
